89a36989946fccd55d4bbed05b49cee26d5504c2
urvi kalia <urvikalia@gmail.com>
2025-07-24 14:53:48 +0530

    implemented hoarse

---
+++ leetcode/src/main/java/org/practice/sorting/Hoares.java
@@ -0,0 +1,47 @@
+package org.practice.sorting;
+
+import java.util.Arrays;
+
+public class Hoares {
+
+    public static void swap(int []arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void sort(int []arr, int start , int end)
+    {
+        if(start >= end) return;
+
+        int pivot = arr[start];
+        int smaller = start+1;
+        int bigger = end-1;
+        while (smaller <= bigger)
+        {
+            if (arr[smaller] < pivot) {
+                smaller++;
+            }
+            else if (arr[bigger] > pivot) {
+                bigger--;
+            }
+            else {
+                swap(arr, smaller, bigger);
+                smaller++;
+                bigger--;
+            }
+        }
+        swap(arr, start, bigger);
+
+        sort(arr, start , bigger-1);
+        sort(arr, bigger+1, end);
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 8, 1, 3, 2};
+        sort(arr,0,arr.length);
+        System.out.println("Sorted array....");
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
